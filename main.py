071ec5bed438fac79c3f16626f112b9c935b2d72
NoMessages <50005075+NoMessages@users.noreply.github.com>
2023-07-21 11:33:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -124,15 +124,21 @@ def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
   return delta.days
 
+wea, wendu, low, high, aqi, fl, notice = get_weather();
 
 # 1. 展示今日
 def get_daliy_time_desc():
   return "今天是："+str(_datetime.datetime.now()).split(" ")[0]+ get_week_day(_datetime.datetime.now())+"\n";
 # 2. 天气
-def get_weather_desc():
-  wea, wendu, low, high, aqi, fl, notice = get_weather();
-  return "今日天气："+wea+",当前温度:"+wendu+",今日最高温度："+high+",今日最低温度："+low+",风向等级："+fl+",今日小tips："+notice+",空气质量指数为："+str(aqi);
-# 3.生日统计,
+def get_weather_desc_1():
+  return "今日天气："+wea+",当前温度:"+wendu;
+def get_weather_desc_2():
+  return "最高温度："+high+",最低温度："+low;
+def get_weather_desc_3():
+  return "风向等级："+fl+",今日小tips："+str(aqi);
+def get_weather_desc_4():
+  return "小tips:" + notice;
+# 3.生日统计
 def get_birthday(d, date_times):
   return "距离生日小美女还有："+str(d.days)+"天"+str(date_times[0])+"小时"+str(date_times[1])+"分钟";
 # 4.除夕倒计时
@@ -140,7 +146,7 @@ def get_chuxi_days(cx_d, data_chuxi):
   return "距离除夕还有："+str(cx_d.days)+"天"+str(data_chuxi[0])+"小时"+str(data_chuxi[1])+"分钟";
 # 5.好吃推荐
 def get_words():
-  return "中午不知道吃啥？菜单参考：-----》【红烧肉】【蒜香油麦菜】【爆炒午餐肉】【炝拌干豆腐丝】【鱼香肉丝】【番茄炒蛋】【茄子炒肉沫】【韭菜炒蛋】【酱烧豆腐】【排骨饭】"
+  return "【红烧肉】【蒜香油麦菜】【爆炒午餐肉】【炝拌干豆腐丝】【鱼香肉丝】【番茄炒蛋】【茄子炒肉沫】【韭菜炒蛋】【酱烧豆腐】【排骨饭】"
 # 6.周年函数
 def get_cele_desc():
   return "已经在一起"+str(get_total_year())+"周年"+str(get_count())+"天哒,还有"+str(get_cele_date())+"天开启第"+str((get_total_year()+1))+"周年~";
@@ -148,7 +154,10 @@ def get_cele_desc():
 client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
 data = {"today":{"value":get_daliy_time_desc()},
-"weather":{"value":get_weather_desc()},
+"weather1":{"value":get_weather_desc_1()},
+"weather2":{"value":get_weather_desc_2()},
+"weather3":{"value":get_weather_desc_3()},
+"weather4":{"value":get_weather_desc_4()},
 "birthday": {"value":get_birthday(d,date_times)},
 "newyear": {"value":get_chuxi_days(cx_d, data_chuxi)},
 "words": {"value":get_words()},
